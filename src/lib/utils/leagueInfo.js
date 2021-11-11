f857308758febc98190f3235b1461db4848e9394
fcboyd <94083973+fcboyd@users.noreply.github.com>
2021-11-11 10:03:12 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -212,7 +212,7 @@ export const managers = [
        "favoriteTeam": "hou", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "TBD", // Can be anything (usually your rival's name)
+         name: "Myself", // Can be anything (usually your rival's name)
          link: 1, // manager array number within this array, or null to link back to all managers page
          image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
