5f3495ff185a84624ee3479738735682b67412e6
fcboyd <94083973+fcboyd@users.noreply.github.com>
2022-10-20 15:35:00 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "784604488257425408"; // your league ID
-export const leagueName = "The League of Below Average Managers"; // your league name
+export const leagueName = "Churros and Tarts"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
